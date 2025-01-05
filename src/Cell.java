d2577ad49e0f13b54de7d2045846e09de7c1b29c
97254 <tamartuil1@gmail.com>
2025-01-05 18:35:40 +0200

    start 1

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,51 @@
 public class Cell {
+
+    /**
+     * this function checks if a given string is a number
+     * @param str
+     * @return true when str is number
+     */
+
+    //isNumber//
+    public static boolean isNumber(String str){
+    return true;
+    }
+
+    //isFormula//
+
+    /**
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isFormula(String str) {
+        if (!str.matches("[A-Z0-9+\\-*/().\\s]*")) {
+            return false;
+
+        }
+    }
+
+
+    //isCell//
+
+    /**
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isCell(String str) {
+        return str.matches("[A-Z]\\d+");
+    }
+
+    //isTaxt
+
+    /**
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isText(String str) {
+        return !str.matches(String.valueOf(isFormula(str) && isNumber(str)));
+    }
+
 }
